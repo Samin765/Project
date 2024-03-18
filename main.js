ea2f89bf7c7d1eab8bf854c7f71bc8505a2003bc
unknown <saminc@kth.se>
2024-03-18 01:19:33 +0100

    main

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("hello");
+
+var string = 'hello';
+
+document.getElementById('box2').innerHTML = "asjdksad";
+
+document.getElementById('box2').addEventListener('click', function(){
+    alert("got clicked");
+});
